29786a7c6644dcad011c3fe12970793734b8fbed
Mike Harris <mike@mbharris.co.uk>
2017-05-09 06:34:45 +0100

    Build ExportAttendees

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cobc -x -free -std=default -o bams bams.cbl createAuthCode.cbl ListAttendeesScreen.cbl
 cobc -std=default -x -free ImportAttendees.cbl
+cobc -std=default -x -free ExportAttendees.cbl
