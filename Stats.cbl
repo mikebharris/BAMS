842373f9f21bc815e436fc077d5e78acab8118be
Mike B Harris <mike.harris@xtreamlab.net>
2017-05-25 00:33:18 +0100

    Compute average paid

--- Stats.cbl
+++ Stats.cbl
@@ -6,21 +6,31 @@ working-storage section.
     01 TotalPaid pic 9(4) value zero.
     01 TotalToPay pic 9(4) value zero.
     01 TotalIncome pic 9(4) value zero.
+    01 NumberOfAttendees pic 9(3) value zero.
+    01 AveragePaid pic 99v99 value zero.
+    01 IgnoredValue pic 9(4).
 
-    01 FigureOutput pic z,z99.
+    01 FigureOutput pic z,z99.99.
 
 procedure division.
     call "Attendees"
     call "FinancialStats" using by reference TotalPaid, TotalToPay
 
+    display spaces
     move TotalPaid to FigureOutput
     display "Total paid is:   " FigureOutput
     move TotalToPay to FigureOutput
     display "Total to pay is: " FigureOutput
     add TotalPaid to TotalToPay giving TotalIncome
     move TotalIncome to FigureOutput
-    display "-----------------------"
+    display "-------------------------"
     display "Total income is: " FigureOutput
+
+    call "AttendeeStats" using by reference NumberOfAttendees, IgnoredValue, IgnoredValue, IgnoredValue, IgnoredValue
+    divide TotalIncome by NumberOfAttendees giving AveragePaid rounded mode is away-from-zero
+    display spaces
+    display "Average paid is:    " AveragePaid
+    display spaces
 
     exit program
     .
